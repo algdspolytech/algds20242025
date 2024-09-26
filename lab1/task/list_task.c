ea24479451fc53d94501e08d5db9e341e88e2cd8
IvaKorsya <korsaeva24@gmail.com>
2024-09-26 17:20:35 +0400

    project structure created

---
+++ lab1/task/list_task.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <list.c>
+int main(){
+    printf ("this is a test");
+    return 0;
+}
---
+++ lab1/tests/list_tests.cpp
@@ -0,0 +1,5 @@
+#include <list.c>
+#include <gtest/gtest.h>
+TEST(first,test){
+    ASSERT_EQ(0,0);
+}
