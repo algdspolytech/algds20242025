503fb33de1b7e37c3f74ed712a782d8a9903c9a5
Egor Gutsalov <egor.gutsalov@gmail.com>
2024-10-16 01:16:33 +0300

    all tests ready

--- test/join.cpp
+++ test/join.cpp
@@ -6,12 +6,104 @@ extern "C"
     #include "list.h"
 }
 
-TEST(join_check, hello_world)
+
+TEST(join_check, normal_case)
+{
+    List* list = initList();
+    addToList(list, "What");
+    addToList(list, " a");
+    addToList(list, " beautiful");
+    addToList(list, " world!");
+    char* res = join(list);
+    char exp[] = "What a beautiful world!";
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, eq_strings)
 {
     List* list = initList();
     addToList(list, "Hello ");
-    addToList(list, "World!");
+    addToList(list, "Hello ");
+    char exp[] = "Hello Hello ";
+    char* res = join(list);
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, empty_list)
+{
+    List* list = initList();
+    char exp[] = "";
+    char* res = join(list);
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, one_string)
+{
+    List* list = initList();
+    addToList(list, "Hello");
+    char exp[] = "Hello";
+    char* res = join(list);
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, empty_string)
+{
+    List* list = initList();
+    addToList(list, "");
+    char exp[] = "";
+    char* res = join(list);
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, empty_strings)
+{
+    List* list = initList();
+    addToList(list, "");
+    addToList(list, "");
+    char exp[] = "";
+    char* res = join(list);
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, special_symbols)
+{
+    List* list = initList();
+    addToList(list, "!@#$%^&*(){}|/~`");
+    addToList(list, "!@#$%^&*(){}|/~`");
+    char exp[] = "!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`";
+    char* res = join(list);
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, different_lens)
+{
+    List* list = initList();
+    addToList(list, "Hello");
+    addToList(list, " World!");
     char exp[] = "Hello World!";
     char* res = join(list);
     ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, long_strings)
+{
+    List* list = initList();
+    addToList(list, "!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`");
+    addToList(list, "!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`");
+    char exp[] = "!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`!@#$%^&*(){}|/~`";
+    char* res = join(list);
+    ASSERT_EQ(strcmp(exp, res), 0);
+}
+
+TEST(join_check, many_strings)
+{
+    List* list = initList();
+    std::string exp1 = "";
+    for(int i = 0; i < 100; ++i)
+    {
+         addToList(list, "Hello ");
+         exp1 += "Hello ";
+    }
+    std::string res(join(list));
+    ASSERT_EQ(res == exp1, true);
 }
--- test/list.cpp
+++ test/list.cpp
@@ -5,8 +5,39 @@ extern "C"
     #include "list.h"
 }
 
-TEST(join_check, ok)
+TEST(join_check, add_one)
 {
     List* list = initList();
     ASSERT_EQ(addToList(list, "AsjdfhjashdjasjkdhaS"), 1);
-    }
+}
+
+TEST(join_check, add_second)
+{
+    List* list = initList();
+    addToList(list, "AsjdfhjashdjasjkdhaS");
+    ASSERT_EQ(addToList(list, "AsjdfhjashdjasjkdhaS"), 1);
+}
+
+TEST(join_check, add_many)
+{
+    List* list = initList();
+    ASSERT_EQ(addToList(list, "Hello"), 1);
+    ASSERT_EQ(addToList(list, "beautiful"), 1);
+    ASSERT_EQ(addToList(list, "world!"), 1);
+}
+
+TEST(join_check, len_correct)
+{
+    List* list = initList();
+    char elem[] = "Hello";
+    addToList(list, elem);
+    ASSERT_EQ(list->len == 5, true);
+}
+
+TEST(join_check, add_equal)
+{
+    List* list = initList();
+    ASSERT_EQ(addToList(list, "duplicate"), 1);
+    ASSERT_EQ(addToList(list, "duplicate"), 1);
+
+}
