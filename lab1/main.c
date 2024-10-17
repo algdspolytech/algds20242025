8c558ecd3d0ed464e5a5abef06b43e5670f7aa2f
Artem Nikonorov <nukonopov@gmail.com>
2024-10-17 11:38:40 +0300

    Внесены правки

--- lab1/main.c
+++ lab1/main.c
@@ -1,16 +1,19 @@
 #include "Header.h"
-#pragma warning(disable:4996)
 
-Set* initialization() { //инициализация множества
-	Set* init = (Set*)malloc(sizeof(node));
+Set *initialization()
+{
+	Set *init = (Set *)malloc(sizeof(node));
 	init->head = NULL;
 	return init;
 }
 
-bool contains(Set* data, int value) { //проверка на уникальность
-	node* iterator = data->head;
-	while (iterator != NULL) {
-		if (iterator->data == value) {
+bool contains(Set *data, int value)
+{
+	node *iterator = data->head;
+	while (iterator != NULL)
+	{
+		if (iterator->data == value)
+		{
 			return true;
 		}
 		iterator = iterator->next;
@@ -18,27 +21,35 @@ bool contains(Set* data, int value) { //проверка на уникальность
 	return false;
 }
 
-void add(Set* set, int data) { //добавление элемента в множество
-	if (!contains(set, data)) {
-		node* ptr = (node*)malloc(sizeof(node));
+void add(Set *set, int data)
+{
+	if (!contains(set, data))
+	{
+		node *ptr = (node *)malloc(sizeof(node));
 		ptr->data = data;
 		ptr->next = set->head;
 		set->head = ptr;
 	}
 }
 
-bool removee(Set* set, int value) {
-	if (set == NULL || set->head == NULL) return false;
+bool removee(Set *set, int value)
+{
+	if (set == NULL || set->head == NULL)
+		return false;
 
-	node* current = set->head;
-	node* previous = NULL;
+	node *current = set->head;
+	node *previous = NULL;
 
-	while (current != NULL) {
-		if (current->data == value) {
-			if (previous == NULL) {
+	while (current != NULL)
+	{
+		if (current->data == value)
+		{
+			if (previous == NULL)
+			{
 				set->head = current->next;
 			}
-			else {
+			else
+			{
 				previous->next = current->next;
 			}
 			free(current);
@@ -50,27 +61,32 @@ bool removee(Set* set, int value) {
 	return false;
 }
 
-
-Set* union_sets(Set* s1, Set* s2) {
-	Set* res = initialization();
-	node* current = s1->head;
-	while (current != NULL) {
+Set *union_sets(Set *s1, Set *s2)
+{
+	Set *res = initialization();
+	node *current = s1->head;
+	while (current != NULL)
+	{
 		add(res, current->data);
 		current = current->next;
 	}
 	current = s2->head;
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		add(res, current->data);
 		current = current->next;
 	}
 	return res;
 }
 
-Set* intersection(Set* s1, Set* s2) {
-	Set* res = initialization();
-	node* current = s1->head;
-	while (current != NULL) {
-		if (contains(s2, current->data)) {
+Set *intersection(Set *s1, Set *s2)
+{
+	Set *res = initialization();
+	node *current = s1->head;
+	while (current != NULL)
+	{
+		if (contains(s2, current->data))
+		{
 			add(res, current->data);
 		}
 		current = current->next;
@@ -78,10 +94,12 @@ Set* intersection(Set* s1, Set* s2) {
 	return res;
 }
 
-Set* free_set(Set* s) {
-	node* current = s->head;
-	while (current != NULL) {
-		node* tmp = current;
+Set *free_set(Set *s)
+{
+	node *current = s->head;
+	while (current != NULL)
+	{
+		node *tmp = current;
 		current = current->next;
 		free(tmp);
 	}
@@ -90,10 +108,12 @@ Set* free_set(Set* s) {
 	return 0;
 }
 
-void print_set(Set* set) {
-	node* current = set->head;
+void print_set(Set *set)
+{
+	node *current = set->head;
 	printf("{ ");
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		printf("%d ", current->data);
 		current = current->next;
 	}
