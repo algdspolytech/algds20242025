beb9e911047d529d1890fe72429ec10231be0523
Motusira <91193170+motusira@users.noreply.github.com>
2024-10-19 23:36:58 +0300

    Update graph.h

--- labc/include/graph.h
+++ labc/include/graph.h
@@ -11,7 +11,5 @@ typedef struct {
 int compare_lm(const graph_list *l, const graph_matrix *m);
 graph_list parse_list(const char *file_name);
 graph_matrix parse_matrix(const char *file_name);
-void add_elemet_l(graph_list *l, int *el);
-void add_element_m(graph_matrix *m, int *el);
 void del_l(graph_list *l);
 void del_m(graph_matrix *m);
