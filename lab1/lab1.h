460a9741e856f3cbef96bfa369d8cddf74ee3ad7
Андреев Александр <andreev0aleksandr0@gmail.com>
2024-10-28 19:23:47 +0300

    f

--- lab1/lab1.h
+++ lab1/lab1.h
@@ -1,8 +1,30 @@
 //
 // Created by Александр on 28.10.2024.
 //
+#pragma once
 
-#ifndef LAB1_H
-#define LAB1_H
+#include <limits.h>
+#define ERROR INT_MIN
 
-#endif //LAB1_H
+typedef struct list_t {
+    int data;
+    struct list_t* next;
+} list_t;
+
+typedef struct {
+    int last;
+    int capacity;
+    int* array;
+}array_t;
+
+list_t* Push_LinkedStack(list_t* p, int data);
+list_t* Pop_LinkedStack(list_t* p);
+int Peek_LinkedStack(list_t* p);
+int Push_ArrayStack(array_t* p, int data);
+int Pop_ArrayStack(array_t* p);
+int Peek_ArrayStack(array_t* p);
+array_t* Create_ArrayStack(int capacity);
+int IsFull_ArrayStack(array_t* p);
+int IsEmpty_ArrayStack(array_t* p);
+int IncreazeCapacity_ArrayStack(array_t* p);
+void Free_ArrayStack(array_t* p);
