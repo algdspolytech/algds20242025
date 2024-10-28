5b14c7b5ace045c3ced04417d578e8d2a17bb417
Андреев Александр <andreev0aleksandr0@gmail.com>
2024-10-28 19:18:48 +0300

    Вариант, о лабе

---
+++ lab1/lab1.h
@@ -0,0 +1,8 @@
+//
+// Created by Александр on 28.10.2024.
+//
+
+#ifndef LAB1_H
+#define LAB1_H
+
+#endif //LAB1_H
---
+++ lab1/test.h
@@ -0,0 +1,8 @@
+//
+// Created by Александр on 28.10.2024.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+#endif //TEST_H
