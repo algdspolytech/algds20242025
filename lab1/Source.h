256a3a99b0f9ae87d8420910a449a41fd630e898
HiSuSlik <95129259+HiSuSlik@users.noreply.github.com>
2024-11-02 16:31:27 +0300

    Add files via upload

---
+++ lab1/Source.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <limits.h>
+#define ERROR INT_MIN
+
+typedef struct list_t {
+    int data;
+    struct list_t* next;
+} list_t;
+
+typedef struct {
+    int last;
+    int capacity;
+    int* array;
+}array_t;
+
+list_t* Push_LinkedStack(list_t* p, int data);
+list_t* Pop_LinkedStack(list_t* p);
+int Peek_LinkedStack(list_t* p);
+int Push_ArrayStack(array_t* p, int data);
+int Pop_ArrayStack(array_t* p);
+int Peek_ArrayStack(array_t* p);
+array_t* Create_ArrayStack(int capacity);
+int IsFull_ArrayStack(array_t* p);
+int IsEmpty_ArrayStack(array_t* p);
+int IncreazeCapacity_ArrayStack(array_t* p);
+void Free_ArrayStack(array_t* p);
