a16c00e850f4eb30c0bba63224fb20bb97b54131
Sergey <167028015+sirgay1@users.noreply.github.com>
2024-11-03 01:35:54 +0300

    Update tests.c

--- lab1/tests.c
+++ lab1/tests.c
@@ -9,7 +9,7 @@
 void Test_init_file_is_null_No1(void)
 {
 	assert(init("test.exe"));
-	printf("\nТест 1 пройден\n");
+	printf("\n1Test passed\n");
 }
 
 void Test_adding_count_more_than_const_No2(void)
@@ -18,7 +18,7 @@ void Test_adding_count_more_than_const_No2(void)
 	Full_name people_to_test[MAX_LEN_FILE];
 
 	assert(adding(file, people_to_test));
-	printf("\nТест 2 пройден\n");
+	printf("\n2Test passed\n");
 }
 
 void Test_adding_empty_file_No3(void)
@@ -27,106 +27,106 @@ void Test_adding_empty_file_No3(void)
 	Full_name people_to_test[MAX_LEN_FILE];
 
 	assert(adding(file, people_to_test) == 0);
-	printf("Тест 3 пройден\n");
+	printf("3Test passed\n");
 }
 
 void Test_compare_patronymic_No4(void)
 {
-	Full_name person1 = { "Иванов", "Иван", "Иванович" };
-	Full_name person2 = { "Иванов", "Иван", "Яковлевич" };
+	Full_name person1 = { "Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "Г€ГўГ Г­Г®ГўГЁГ·" };
+	Full_name person2 = { "Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "ГџГЄГ®ГўГ«ГҐГўГЁГ·" };
 
 	assert(compare(&person1, &person2) < 0);
-	printf("Тест 4 пройден\n");
+	printf("4Test passed\n");
 }
 
 void Test_compare_name_No5(void)
 {
-	Full_name person1 = { "Иванов", "Иван", "Иванович" };
-	Full_name person2 = { "Иванов", "Дмитрий", "Яковлевич" };
+	Full_name person1 = { "Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "Г€ГўГ Г­Г®ГўГЁГ·" };
+	Full_name person2 = { "Г€ГўГ Г­Г®Гў", "Г„Г¬ГЁГІГ°ГЁГ©", "ГџГЄГ®ГўГ«ГҐГўГЁГ·" };
 
 	assert(compare(&person1, &person2) > 0);
-	printf("Тест 5 пройден\n");
+	printf("5Test passed\n");
 }
 
 void Test_compare_surname_No6(void)
 {
-	Full_name person1 = { "Иванов", "Иван", "Иванович" };
-	Full_name person2 = { "Авдеев", "Дмитрий", "Яковлевич" };
+	Full_name person1 = { "Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "Г€ГўГ Г­Г®ГўГЁГ·" };
+	Full_name person2 = { "ГЂГўГ¤ГҐГҐГў", "Г„Г¬ГЁГІГ°ГЁГ©", "ГџГЄГ®ГўГ«ГҐГўГЁГ·" };
 
 	assert(compare(&person1, &person2) > 0);
-	printf("Тест 6 пройден\n");
+	printf("6Test passed\n");
 }
 
 void Test_find_surname_Iv_No7(void)
 {
 	Full_name people[] =
 	{
-	   {"Иванов", "Иван", "Иванович"},
-	   {"Петров", "Иван", "Петрович"},
-	   {"Сидоров", "Сергей", "Сергеевич"},
-	   {"Иванова", "Анна", "Петровна"}
+	   {"Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "Г€ГўГ Г­Г®ГўГЁГ·"},
+	   {"ГЏГҐГІГ°Г®Гў", "Г€ГўГ Г­", "ГЏГҐГІГ°Г®ГўГЁГ·"},
+	   {"Г‘ГЁГ¤Г®Г°Г®Гў", "Г‘ГҐГ°ГЈГҐГ©", "Г‘ГҐГ°ГЈГҐГҐГўГЁГ·"},
+	   {"Г€ГўГ Г­Г®ГўГ ", "ГЂГ­Г­Г ", "ГЏГҐГІГ°Г®ГўГ­Г "}
 	};
 	int count = 4;
 	char found_surnames[10][50];
 	int found_count;
 
-	found_count = find_testable(people, count, "Ив", "", "", found_surnames);
+	found_count = find_testable(people, count, "Г€Гў", "", "", found_surnames);
 
 	assert(found_count == 2);
-	assert(strcmp(found_surnames[0], "Иванов") == 0);
-	assert(strcmp(found_surnames[1], "Иванова") == 0);
-	printf("Тест 7 пройден\n");
+	assert(strcmp(found_surnames[0], "Г€ГўГ Г­Г®Гў") == 0);
+	assert(strcmp(found_surnames[1], "Г€ГўГ Г­Г®ГўГ ") == 0);
+	printf("7Test passed\n");
 }
 
 void Test_find_complete_match_No8(void)
 {
 	Full_name people[] =
 	{
-	   {"Иванов", "Иван", "Иванович"},
-	   {"Петров", "Иван", "Петрович"},
-	   {"Сидоров", "Сергей", "Сергеевич"},
-	   {"Иванова", "Анна", "Петровна"}
+	   {"Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "Г€ГўГ Г­Г®ГўГЁГ·"},
+	   {"ГЏГҐГІГ°Г®Гў", "Г€ГўГ Г­", "ГЏГҐГІГ°Г®ГўГЁГ·"},
+	   {"Г‘ГЁГ¤Г®Г°Г®Гў", "Г‘ГҐГ°ГЈГҐГ©", "Г‘ГҐГ°ГЈГҐГҐГўГЁГ·"},
+	   {"Г€ГўГ Г­Г®ГўГ ", "ГЂГ­Г­Г ", "ГЏГҐГІГ°Г®ГўГ­Г "}
 	};
 	int count = 4;
 	char found_surnames[10][50];
 	int found_count;
 
-	found_count = find_testable(people, count, "Иванов", "Иван", "Иванович", found_surnames);
+	found_count = find_testable(people, count, "Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "Г€ГўГ Г­Г®ГўГЁГ·", found_surnames);
 
 	assert(found_count == 1);
-	assert(strcmp(found_surnames[0], "Иванов") == 0);
-	printf("Тест 8 пройден\n");
+	assert(strcmp(found_surnames[0], "Г€ГўГ Г­Г®Гў") == 0);
+	printf("8Test passed\n");
 }
 
 void Test_find_no_matches_No9(void)
 {
 	Full_name people[] =
 	{
-	   {"Иванов", "Иван", "Иванович"},
-	   {"Петров", "Иван", "Петрович"},
-	   {"Сидоров", "Сергей", "Сергеевич"},
-	   {"Иванова", "Анна", "Петровна"}
+	   {"Г€ГўГ Г­Г®Гў", "Г€ГўГ Г­", "Г€ГўГ Г­Г®ГўГЁГ·"},
+	   {"ГЏГҐГІГ°Г®Гў", "Г€ГўГ Г­", "ГЏГҐГІГ°Г®ГўГЁГ·"},
+	   {"Г‘ГЁГ¤Г®Г°Г®Гў", "Г‘ГҐГ°ГЈГҐГ©", "Г‘ГҐГ°ГЈГҐГҐГўГЁГ·"},
+	   {"Г€ГўГ Г­Г®ГўГ ", "ГЂГ­Г­Г ", "ГЏГҐГІГ°Г®ГўГ­Г "}
 	};
 	int count = 4;
 	char found_surnames[10][50];
 	int found_count;
 
-	found_count = find_testable(people, count, "Николаев", "Николай", "Николаевич", found_surnames);
+	found_count = find_testable(people, count, "ГЌГЁГЄГ®Г«Г ГҐГў", "ГЌГЁГЄГ®Г«Г Г©", "ГЌГЁГЄГ®Г«Г ГҐГўГЁГ·", found_surnames);
 
 	assert(found_count == 0);
-	printf("Тест 9 пройден\n");
+	printf("9Test passed\n");
 }
 
 void Test_adding_invalid_format_string_No10(void)
 {
 	FILE* file = fopen("data_to_test10.txt", "w+");
-	fprintf(file, "Иванов Иван Иванович\nНекорректнаяСтрока\nПетров Петр Петрович\n");
+	fprintf(file, "Г€ГўГ Г­Г®Гў Г€ГўГ Г­ Г€ГўГ Г­Г®ГўГЁГ·\nГЌГҐГЄГ®Г°Г°ГҐГЄГІГ­Г ГїГ‘ГІГ°Г®ГЄГ \nГЏГҐГІГ°Г®Гў ГЏГҐГІГ° ГЏГҐГІГ°Г®ГўГЁГ·\n");
 	rewind(file);
 
 	Full_name arr[MAX_LEN_FILE];
 
 	int count = adding(file, arr);
 	assert(count == 2);
-	printf("Тест 10 пройден\n");
+	printf("10Test passed\n");
 	remove("test_file_3.txt");
 }
