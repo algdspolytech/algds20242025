1497f6bf979f703f0aa7b64358dbf14e74a12ae6
Sergey <167028015+sirgay1@users.noreply.github.com>
2024-11-03 01:40:59 +0300

    Update tests.c

--- lab1/tests.c
+++ lab1/tests.c
@@ -32,8 +32,8 @@ void Test_adding_empty_file_No3(void)
 
 void Test_compare_patronymic_No4(void)
 {
-	Full_name person1 = { "Èâàíîâ", "Èâàí", "Èâàíîâè÷" };
-	Full_name person2 = { "Èâàíîâ", "Èâàí", "ßêîâëåâè÷" };
+	Full_name person1 = { "Иванов", "Иван", "Иванович" };
+	Full_name person2 = { "Иванов", "Иван", "Яковлевич" };
 
 	assert(compare(&person1, &person2) < 0);
 	printf("4Test passed\n");
@@ -41,8 +41,8 @@ void Test_compare_patronymic_No4(void)
 
 void Test_compare_name_No5(void)
 {
-	Full_name person1 = { "Èâàíîâ", "Èâàí", "Èâàíîâè÷" };
-	Full_name person2 = { "Èâàíîâ", "Äìèòðèé", "ßêîâëåâè÷" };
+	Full_name person1 = { "Иванов", "Иван", "Иванович" };
+	Full_name person2 = { "Иванов", "Дмитрий", "Яковлевич" };
 
 	assert(compare(&person1, &person2) > 0);
 	printf("5Test passed\n");
@@ -50,8 +50,8 @@ void Test_compare_name_No5(void)
 
 void Test_compare_surname_No6(void)
 {
-	Full_name person1 = { "Èâàíîâ", "Èâàí", "Èâàíîâè÷" };
-	Full_name person2 = { "Àâäååâ", "Äìèòðèé", "ßêîâëåâè÷" };
+	Full_name person1 = { "Иванов", "Иван", "Иванович" };
+	Full_name person2 = { "Авдеев", "Дмитрий", "Яковлевич" };
 
 	assert(compare(&person1, &person2) > 0);
 	printf("6Test passed\n");
@@ -61,20 +61,20 @@ void Test_find_surname_Iv_No7(void)
 {
 	Full_name people[] =
 	{
-	   {"Èâàíîâ", "Èâàí", "Èâàíîâè÷"},
-	   {"Ïåòðîâ", "Èâàí", "Ïåòðîâè÷"},
-	   {"Ñèäîðîâ", "Ñåðãåé", "Ñåðãååâè÷"},
-	   {"Èâàíîâà", "Àííà", "Ïåòðîâíà"}
+	   {"Иванов", "Иван", "Иванович"},
+	   {"Петров", "Иван", "Петрович"},
+	   {"Сидоров", "Сергей", "Сергеевич"},
+	   {"Иванова", "Анна", "Петровна"}
 	};
 	int count = 4;
 	char found_surnames[10][50];
 	int found_count;
 
-	found_count = find_testable(people, count, "Èâ", "", "", found_surnames);
+	found_count = find_testable(people, count, "Ив", "", "", found_surnames);
 
 	assert(found_count == 2);
-	assert(strcmp(found_surnames[0], "Èâàíîâ") == 0);
-	assert(strcmp(found_surnames[1], "Èâàíîâà") == 0);
+	assert(strcmp(found_surnames[0], "Иванов") == 0);
+	assert(strcmp(found_surnames[1], "Иванова") == 0);
 	printf("7Test passed\n");
 }
 
@@ -82,19 +82,19 @@ void Test_find_complete_match_No8(void)
 {
 	Full_name people[] =
 	{
-	   {"Èâàíîâ", "Èâàí", "Èâàíîâè÷"},
-	   {"Ïåòðîâ", "Èâàí", "Ïåòðîâè÷"},
-	   {"Ñèäîðîâ", "Ñåðãåé", "Ñåðãååâè÷"},
-	   {"Èâàíîâà", "Àííà", "Ïåòðîâíà"}
+	   {"Иванов", "Иван", "Иванович"},
+	   {"Петров", "Иван", "Петрович"},
+	   {"Сидоров", "Сергей", "Сергеевич"},
+	   {"Иванова", "Анна", "Петровна"}
 	};
 	int count = 4;
 	char found_surnames[10][50];
 	int found_count;
 
-	found_count = find_testable(people, count, "Èâàíîâ", "Èâàí", "Èâàíîâè÷", found_surnames);
+	found_count = find_testable(people, count, "Иванов", "Иван", "Иванович", found_surnames);
 
 	assert(found_count == 1);
-	assert(strcmp(found_surnames[0], "Èâàíîâ") == 0);
+	assert(strcmp(found_surnames[0], "Иванов") == 0);
 	printf("8Test passed\n");
 }
 
@@ -102,16 +102,16 @@ void Test_find_no_matches_No9(void)
 {
 	Full_name people[] =
 	{
-	   {"Èâàíîâ", "Èâàí", "Èâàíîâè÷"},
-	   {"Ïåòðîâ", "Èâàí", "Ïåòðîâè÷"},
-	   {"Ñèäîðîâ", "Ñåðãåé", "Ñåðãååâè÷"},
-	   {"Èâàíîâà", "Àííà", "Ïåòðîâíà"}
+	   {"Иванов", "Иван", "Иванович"},
+	   {"Петров", "Иван", "Петрович"},
+	   {"Сидоров", "Сергей", "Сергеевич"},
+	   {"Иванова", "Анна", "Петровна"}
 	};
 	int count = 4;
 	char found_surnames[10][50];
 	int found_count;
 
-	found_count = find_testable(people, count, "Íèêîëàåâ", "Íèêîëàé", "Íèêîëàåâè÷", found_surnames);
+	found_count = find_testable(people, count, "Николаев", "Николай", "Николаевич", found_surnames);
 
 	assert(found_count == 0);
 	printf("9Test passed\n");
@@ -120,7 +120,7 @@ void Test_find_no_matches_No9(void)
 void Test_adding_invalid_format_string_No10(void)
 {
 	FILE* file = fopen("data_to_test10.txt", "w+");
-	fprintf(file, "Èâàíîâ Èâàí Èâàíîâè÷\nÍåêîððåêòíàÿÑòðîêà\nÏåòðîâ Ïåòð Ïåòðîâè÷\n");
+	fprintf(file, "Иванов Иван Иванович\nНекорректнаяСтрока\nПетров Петр Петрович\n");
 	rewind(file);
 
 	Full_name arr[MAX_LEN_FILE];
