099b7f40271172888d3c43847f2c5db78b452d09
01Aleksandr10 <158523453+01Aleksandr10@users.noreply.github.com>
2024-11-02 23:24:16 +0300

    Delete lab1 directory
    Криво заргузил папку с лабой

--- lab1/lab2.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Александр on 02.11.2024.
-//
-
-#pragma once
-
-#include "gtest/gtest.h"
--- lab1/p.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Александр on 02.11.2024.
-//
-
-#ifndef P_H
-#define P_H
-
-#endif //P_H
