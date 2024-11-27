4c3b0e6ac9be669d2a7e267e4d0a2c344e305f4e
Jarsky-s <daniilsanin2000@gmail.ru>
2024-11-27 15:22:17 +0300

    структуры

--- lab1/main.c
+++ lab1/main.c
@@ -2,12 +2,29 @@
 #include <stdio.h>
 #include <stdint.h>
 
+struct point {
+    int x;
+    int y;
+};
+
+struct point makepoint (int x, int y){
+    struct point temp;
+    temp.x = x;
+    temp.y = y;
+    return temp;
+}
+
+struct point addpoint (struct point p1, struct point p2){
+    p1.x += p2.x;
+    p1.y += p2.y;
+    return p1;
+}
+
 int main(){
-    unsigned int a = 5, b = 9, c = 3;
-    int * ptr_a = &a;
-    int * ptr_b = &b;
-    int *ptr_c = &c;
-    uintptr_t link_b = (uintptr_t)ptr_a^(uintptr_t)ptr_c;
-    printf("%d", *(int *)(link_b ^ (uintptr_t)ptr_a));
+    struct point p1, p2, pt, *pp1 = &p1, *pp2 = &p2, *ppt = &pt;
+    p1 = makepoint(5, 6);
+    p2 = makepoint(5, 4);
+    pt = addpoint(p1, p2);
+    printf ("%d \t %d", pt.x, pt.y);
     return 0;
 }
