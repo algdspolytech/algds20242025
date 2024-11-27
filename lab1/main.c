9a56d0f8e81ce723458c0b1a8b0fe1c24f440ca1
Jarsky-s <daniilsanin2000@gmail.ru>
2024-11-27 20:29:27 +0300

    XOR-linked list готов но не работает

--- lab1/main.c
+++ lab1/main.c
@@ -1,30 +1,127 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <string.h>
 
-struct point {
-    int x;
-    int y;
-};
+typedef struct Node {
+    char * data;
+    struct Node * link;
+}Node;
 
-struct point makepoint (int x, int y){
-    struct point temp;
-    temp.x = x;
-    temp.y = y;
-    return temp;
+Node * XOR_function(Node * a, Node * b){
+    return (Node *)((uintptr_t)a ^ (uintptr_t)b);
 }
 
-struct point addpoint (struct point p1, struct point p2){
-    p1.x += p2.x;
-    p1.y += p2.y;
-    return p1;
+Node * createNode(const char * data){
+    Node * new_node = (Node *)malloc(sizeof(Node));
+    new_node -> link = NULL;
+    new_node -> data = strdup(data);
+    return new_node;
+}
+
+void listInsert(Node ** head, const char * data){
+    Node * new_node = createNode(data);
+    new_node -> link = *head;
+    if(*head){
+        (*head) -> link = XOR_function(new_node, (*head) -> link);
+    }
+    new_node = *head;
+}
+
+Node * find(Node * head, const char * key){
+    Node * previous = NULL;
+    Node * current = head;
+    while (current){
+        if (strcmp(current -> data, key) == 0){
+            return current;
+        }
+        Node * next = XOR_function(previous, current -> link);
+        previous = current;
+        current = next;
+    }
+    return NULL;
+}
+
+void deleteByKey(Node ** head, const char * key){
+    Node * previous = NULL;
+    Node * current = *head;
+    while (current){        
+        if (strcmp(current -> data, key) == 0){
+            Node * next = XOR_function(previous, current -> link);
+            if (current == *head){
+                *head = next;
+            }
+            if (next){
+                next -> link = XOR_function(previous, XOR_function(current, next -> link));
+            }
+            if (previous){
+                previous -> link = XOR_function(XOR_function(current, previous -> link), next);
+            }
+            free(current -> data);
+            free(current);
+            return;
+        }
+        Node * next = XOR_function(previous, current -> link);
+        previous = current;
+        current = next;
+    }
+}
+
+void deleteByAddress(Node ** head, Node * node){
+    if (!node) return;
+    Node * previous = NULL;
+    Node * current = *head;
+    while (current){
+        if (current == node){
+            Node * next = XOR_function(previous, current -> link);
+            if(previous){
+                previous -> link = XOR_function(XOR_function(current, previous -> link), next);
+            }
+            if(next){
+                next -> link = XOR_function(previous, XOR_function(current, next -> link));
+            }
+            if (current == *head){
+                *head = next;
+            }
+            free(current -> data);
+            free (current);
+            return;
+        }
+        Node * next = XOR_function(previous, current -> link);
+        previous = current;
+        current = next;
+    }
+}
+
+void iterate (Node * head){
+    if (!head) return;
+    Node * previous = NULL;
+    Node * current = head;
+    while (current){
+        printf("%s\n", current -> data);
+        Node * next = XOR_function(previous, current -> link);
+        previous = current;
+        current = next;
+    }
+}
+
+void clearList (Node ** head){
+    Node * current = *head;
+    while (current){
+        Node * next = XOR_function(NULL, current -> link);
+        free(current -> data);
+        free(current);
+        current = next;
+    }
+    *head = NULL;
 }
 
 int main(){
-    struct point p1, p2, pt, *pp1 = &p1, *pp2 = &p2, *ppt = &pt;
-    p1 = makepoint(5, 6);
-    p2 = makepoint(5, 4);
-    pt = addpoint(p1, p2);
-    printf ("%d \t %d", pt.x, pt.y);
+    Node * head = NULL;
+    listInsert(&head, "O");
+    listInsert(&head, "D");
+    listInsert(&head, "e");
+    listInsert(&head, "t");
+    iterate(head);
+    clearList(&head);
     return 0;
 }
