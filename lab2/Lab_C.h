e6f78c9947e4e5f4ab3bf53230a6912791ddb0be
Mattheus986 <167245143+Mattheus986@users.noreply.github.com>
2024-12-02 16:19:35 +0300

    Add files via upload

---
+++ lab2/Lab_C.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#define MAX_VERTICES 50
+
+typedef struct {
+    int numVertices;
+    int adjacencyMatrix[MAX_VERTICES][MAX_VERTICES];
+} Graph;
+
+// ��������� BFS
+int shortestPath(const char* filename);
---
+++ lab2/test.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern "C" {
+#include "..\LAB_C1\Lab_C.h"
+}
+
+TEST(Lab_C_Tests, EmptyGraph_DisplayError) {
+	const char* filename = "empty_graph.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
+
+TEST(Lab_C_Tests, NoPathBetweenVertices_ReturnMinus1) {
+	const char* filename = "no_path.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
+
+TEST(Lab_C_Tests, MultiplePathsBetweenVertices_ReturnValidVal) {
+	const char* filename = "multiple_paths.txt";
+	ASSERT_EQ(shortestPath(filename), 2);
+}
+
+TEST(Lab_C_Tests, EqualStartAndEndVertices_Return0) {
+	const char* filename = "same_start_and_end.txt";
+	ASSERT_EQ(shortestPath(filename), 0);
+}
+
+TEST(Lab_C_Tests, NotAppropriateTextFile_DisplayError) {
+	const char* filename = "not_appropriate_format.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
+
+TEST(Lab_C_Tests, InvalidStartVertex_ReturnMinus1) {
+	const char* filename = "invalid_start_vertex.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
+
+TEST(Lab_C_Tests, InvalidNumberOfVertices_ReturnMinus1) {
+	const char* filename = "invalid_num_of_vertices.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
+
+TEST(Lab_C_Tests, InvalidEndVertex_ReturnMinus1) {
+	const char* filename = "invalid_end_vertex.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
+
+TEST(Lab_C_Tests, InvalidAdjacencyMatrix_DisplayError) {
+	const char* filename = "invalid_adjacency_matrix.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
+
+TEST(Lab_C_Tests, InvalidFileName_DisplayError) {
+	const char* filename = "invalid_file_name_hehehehehe.txt";
+	ASSERT_EQ(shortestPath(filename), -1);
+}
