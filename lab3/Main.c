e1247e14685b340a19d62c1f93641db920eea004
Anastasia <95341361+Anleo1@users.noreply.github.com>
2024-12-03 19:03:47 +0300

    Create Main.c

---
+++ lab3/Main.c
@@ -0,0 +1,20 @@
+#include "Maze.h"
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    Maze maze;
+    Point start = { 0, 0 };
+    Point exit = { 5, 5 };
+    int labyrinth[6][6] = {
+        {0, 1, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 1},
+        {1, 0, 1, 0, 0, 0},
+        {1, 0, 1, 0, 1, 0},
+        {1, 0, 1, 0, 1, 0},
+        {1, 0, 0, 0, 1, 0}
+    }; 
+    CreateMaze(&maze, labyrinth);
+    FindWay(&maze, start, exit); 
+    return 0;
+}                      
