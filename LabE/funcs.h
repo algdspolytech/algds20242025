b1fa39c1e61cddb362ffbc5294d3f73a0ed618ad
PonChick3n <aponchick3n420@mail.ru>
2024-12-11 12:51:31 +0300

    Отформатировать код

--- LabE/funcs.h
+++ LabE/funcs.h
@@ -5,20 +5,20 @@
 #include <string.h>
 
 typedef struct TreeNode {
-    int value;
-    int width;
-    struct TreeNode* left;
-    struct TreeNode* right;
+  int value;
+  int width;
+  struct TreeNode *left;
+  struct TreeNode *right;
 } TreeNode;
 
 #ifdef __cplusplus
 
 extern "C" {
 #endif
-    TreeNode* create_node(int value);
-    int calculate_subtree_width(TreeNode* node);
-    void print_tree_with_width(TreeNode* node, int level);
-    void free_tree(TreeNode* node);
+TreeNode *create_node(int value);
+int calculate_subtree_width(TreeNode *node);
+void print_tree_with_width(TreeNode *node, int level);
+void free_tree(TreeNode *node);
 #ifdef __cplusplus
 }
 #endif
--- LabE/test.cpp
+++ LabE/test.cpp
@@ -6,100 +6,100 @@
 using namespace std;
 
 TEST(_calculate_subtree_width, SimpleTree) { /*return True*/
-    TreeNode* root = create_node(10);
-    root->left = create_node(5);
-    root->right = create_node(15);
+  TreeNode *root = create_node(10);
+  root->left = create_node(5);
+  root->right = create_node(15);
 
-    int width = calculate_subtree_width(root);
-    EXPECT_EQ(width, 5);
+  int width = calculate_subtree_width(root);
+  EXPECT_EQ(width, 5);
 
-    free_tree(root);
+  free_tree(root);
 }
 
 TEST(_calculate_subtree_width, SingleNode) { /*return True*/
-    TreeNode* root = create_node(42);
+  TreeNode *root = create_node(42);
 
-    int width = calculate_subtree_width(root);
-    EXPECT_EQ(width, 2);
+  int width = calculate_subtree_width(root);
+  EXPECT_EQ(width, 2);
 
-    free_tree(root);
+  free_tree(root);
 }
 
 TEST(_calculate_subtree_width, NestedNodes) { /*return True*/
-    TreeNode* root = create_node(100);
-    root->left = create_node(50);
-    root->right = create_node(25);
-    root->left->left = create_node(5);
-    root->left->right = create_node(10);
+  TreeNode *root = create_node(100);
+  root->left = create_node(50);
+  root->right = create_node(25);
+  root->left->left = create_node(5);
+  root->left->right = create_node(10);
 
-    int width = calculate_subtree_width(root);
-    EXPECT_EQ(width, 10);
+  int width = calculate_subtree_width(root);
+  EXPECT_EQ(width, 10);
 
-    free_tree(root);
+  free_tree(root);
 }
 
 TEST(_calculate_subtree_width, OnlyLeftSubtree) { /*return True*/
-    TreeNode* root = create_node(10);
-    root->left = create_node(20);
+  TreeNode *root = create_node(10);
+  root->left = create_node(20);
 
-    int width = calculate_subtree_width(root);
-    EXPECT_EQ(width, 4);
+  int width = calculate_subtree_width(root);
+  EXPECT_EQ(width, 4);
 
-    free_tree(root);
+  free_tree(root);
 }
 
 TEST(_calculate_subtree_width, OnlyRightSubtree) { /*return True*/
-    TreeNode* root = create_node(10);
-    root->right = create_node(300);
+  TreeNode *root = create_node(10);
+  root->right = create_node(300);
 
-    int width = calculate_subtree_width(root);
-    EXPECT_EQ(width, 5);
+  int width = calculate_subtree_width(root);
+  EXPECT_EQ(width, 5);
 
-    free_tree(root);
+  free_tree(root);
 }
 
 TEST(_calculate_subtree_width, EmptyTree) { /*return True*/
-    TreeNode* root = NULL;
+  TreeNode *root = NULL;
 
-    int width = calculate_subtree_width(root);
-    EXPECT_EQ(width, 0);
+  int width = calculate_subtree_width(root);
+  EXPECT_EQ(width, 0);
 }
 
 TEST(_create_node, CorrectValue) { /*all return True*/
-    TreeNode* node = create_node(25);
-    EXPECT_EQ(node->value, 25);
-    EXPECT_EQ(node->width, 0);
-    free_tree(node);
+  TreeNode *node = create_node(25);
+  EXPECT_EQ(node->value, 25);
+  EXPECT_EQ(node->width, 0);
+  free_tree(node);
 }
 
 TEST(_create_node, InitialPointers) { /*all return True*/
-    TreeNode* node = create_node(42);
-    EXPECT_EQ(node->left, nullptr);
-    EXPECT_EQ(node->right, nullptr);
-    free_tree(node);
+  TreeNode *node = create_node(42);
+  EXPECT_EQ(node->left, nullptr);
+  EXPECT_EQ(node->right, nullptr);
+  free_tree(node);
 }
 
 TEST(_free_tree, SingleNode) { /*return True*/
-    TreeNode* node = create_node(99);
-    free_tree(node);
-    SUCCEED();
+  TreeNode *node = create_node(99);
+  free_tree(node);
+  SUCCEED();
 }
 
 TEST(_free_tree, NestedNodes) { /*return True*/
-    TreeNode* root = create_node(10);
-    root->left = create_node(20);
-    root->right = create_node(30);
-    root->left->left = create_node(40);
-    root->left->right = create_node(50);
+  TreeNode *root = create_node(10);
+  root->left = create_node(20);
+  root->right = create_node(30);
+  root->left->left = create_node(40);
+  root->left->right = create_node(50);
 
-    free_tree(root);
-    SUCCEED();
+  free_tree(root);
+  SUCCEED();
 }
 
-int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
 
-	cout << "RUN TEST" << endl;
+  cout << "RUN TEST" << endl;
 
-	return RUN_ALL_TESTS();
+  return RUN_ALL_TESTS();
 }
