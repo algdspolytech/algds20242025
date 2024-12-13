8eee8fd2e678db8280e20504919f1ace13127bc2
TToJlkoBHuK <28arhalex@gmail.com>
2024-12-13 21:22:20 +0300

    plus test

---
+++ lab1/test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+void test_empty_tree();                   // Test 1
+void test_single_node();                  // Test 2
+void test_left_skewed_tree();             // Test 3
+void test_right_skewed_tree();            // Test 4
+void test_duplicate_length_words();       // Test 5
+void test_long_words();                   // Test 6
+void test_balanced_tree();                // Test 7
+void test_special_characters();           // Test 8
+void test_words_with_spaces();            // Test 9
+void test_large_tree();                   // Test 10
+
+#endif // TEST_H
