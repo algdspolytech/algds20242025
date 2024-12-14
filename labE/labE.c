599c2b87d05d905fb1445f2c728398afbf00143e
ilyagrishencko <ilya.grishencko2018@yandex.ru>
2024-12-15 00:21:30 +0300

    Update labE.c

--- labE/labE.c
+++ labE/labE.c
@@ -3,216 +3,174 @@
 #include <string.h>
 #include <assert.h>
 #include <stdbool.h>
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 1000
 
-typedef struct node {
+typedef struct Tree {
     char* data;
-    struct node* prev;
-    struct node* next;
-} Node;
+    struct Tree* left;
+    struct Tree* right;
+} Tree;
 
-void append(Node** head, const char* str) {
-    if (*head == NULL) {
-        Node* new_node = malloc(sizeof(Node));
-        if (!new_node) {
-            perror("Memory allocation error for the node");
-            exit(EXIT_FAILURE);
-        }
-
-        new_node->data = _strdup(str);
-        if (!new_node->data) {
-            free(new_node);
-            perror("Line copying error");
-            exit(EXIT_FAILURE);
-        }
-
-        new_node->prev = NULL;
-        new_node->next = NULL;
-        *head = new_node;
-        return;
-    }
-
-    Node* current = *head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-
-    Node* new_node = malloc(sizeof(Node));
+Tree* createLeaf(const char* word) {
+    Tree* new_node = (Tree*)malloc(sizeof(Tree));
     if (!new_node) {
         perror("Memory allocation error for the node");
         exit(EXIT_FAILURE);
     }
 
-    new_node->data = _strdup(str);
+    new_node->data = _strdup(word);
     if (!new_node->data) {
         free(new_node);
         perror("Line copying error");
         exit(EXIT_FAILURE);
     }
-
-    new_node->prev = current;
-    new_node->next = NULL;
-    current->next = new_node;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
 }
 
-void swapNode(Node* first, Node* second) {
-    char* FirstData = first->data;
-    first->data = second->data;
-    second->data = FirstData;
+Tree* createTree(Tree* root, const char* word) {
+    if (root == NULL) {
+        return createLeaf(word);
+    }
+    if (strcmp(word, root->data) < 0 || (strcmp(word, root->data) == 0 && strlen(word) < strlen(root->data))) {
+        root->left = createTree(root->left, word);
+    }
+    else {
+        root->right = createTree(root->right, word);
+    }
+    return root;
 }
 
-bool bubbleSort(Node** head, bool b) {
-    if (b) {
-        bool swapped = true;
-        while (swapped) {
-            swapped = false;
-            Node* current = *head;
-            while (current->next != NULL) {
-                if (strcmp(current->data, current->next->data) > 0) {
-                    swapNode(current, current->next);
-                    swapped = true;
-                }
-                current = current->next;
-            }
-        }
-        return true;
+int Width(Tree* node) {
+    if (node == NULL) {
+        return 0;
     }
-    else{
-        return false;
+    int leftWidth = Width(node->left);
+    int rightWidth = Width(node->right);
+    return leftWidth + rightWidth + strlen(node->data);
+}
+
+void printTree(Tree* node, FILE* output) {
+    if (node == NULL) {
+        return;
     }
+    printTree(node->left, output);
+    fprintf(output, "%s%d\n", node->data, Width(node));
+    printTree(node->right, output);
+}
+
+void freeTree(Tree* node) {
+    if (node == NULL) {
+        return;
+    }
+    free(node->data);
+    freeTree(node->left);
+    freeTree(node->right);
+    free(node);
 }
 
 int main() {
-    Node* head = NULL;
+    FILE* inputFile = fopen("input.txt", "r");
+    FILE* outputFile = fopen("output.txt", "w");
 
-    FILE* inputFile = fopen("TextFile1.txt", "r");
     if (!inputFile) {
         perror("Error opening the input file");
         return EXIT_FAILURE;
     }
-
-    FILE* outputFile = fopen("TextFile2.txt", "w");
     if (!outputFile) {
         perror("Error opening the output file");
-        fclose(inputFile);
         return EXIT_FAILURE;
     }
 
-    char buffer[BUFFER_SIZE];
-    bool b = false;
+    Tree* root = NULL;
+    char bufferList[BUFFER_SIZE];
 
-    while (fgets(buffer, sizeof(buffer), inputFile)) {
-        b = true;
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-        }
-        append(&head, buffer);
+    while (fgets(bufferList, sizeof(bufferList), inputFile)) {
+        root = createTree(root, bufferList);
     }
-
+    printTree(root, outputFile);
     fclose(inputFile);
-
-        fprintf(outputFile, "Unsorted list: ");
-        for (Node* current = head; current != NULL; current = current->next) {
-            fprintf(outputFile, "%s ", current->data);
-        }
-        fprintf(outputFile, "\n");
-
-        bubbleSort(&head,b);
-
-        fprintf(outputFile, "Sorted list: ");
-        for (Node* current = head; current != NULL; current = current->next) {
-            fprintf(outputFile, "%s ", current->data);
-        }
     fclose(outputFile);
+    freeTree(root);
+    //Unit Tests
+    test_createLeaf(); // Проверка создания листа
+    test_createTree_empty(); // Проверка добавления в пустое дерево
+    test_createTree_noempty_right(); // Проверка добавления в не пустое дерево (вправо)
+    test_createTree_noempty_left(); // Проверка добавления в не пустое дерево (влево)
+    test_Width_oneElement(); // Проверка вычисления ширины поддерева с одним элементом
+    test_Width_nononeElement(); // Проверка вычисления ширины поддерева с более чем одним элементом
+    test_Width_nononeElement_leftList(); // Проверка вычисления ширины поддерева у вырожденного в список по возрастанию
+    test_Width_nononeElement_rightList(); // Проверка вычисления ширины поддерева у вырожденного в список по убыванию
+    test_Width_nononeElement_FullTree(); // Проверка вычисления ширины поддерева у полного дерева
+    test_Width_nononeElement_RandomTree(); // Проверка вычисления ширины поддерева
 
-    while (head != NULL) {
-        Node* temp = head;
-        head = head->next;
-        free(temp->data);
-        free(temp);
-    }
-
-    //Unit tests
-    test_append_empty_list(); //проверка добавления в пустой список
-    test_append_nonempty_list(); //проверка добавления в непустой список
-    test_bubbleSort(); //проверка сортировки для обычного (рандомного) списка
-    test_bubbleSort_empty_list(); //empty_list пустой список
-    test_bubbleSort_sorted_list(); //sorted_list список отсортированный
-    test_bubbleSort_cosorted_list(); //cosorted_list список отсортированный в обратном порядке
-    test_bubbleSort_constant_list(); //constant_list список из одинаковых элементов
-    test_bubbleSort_one_element_list(); //one_element_list список из одного элемента
-    test_swapNode_nonequals(); //проверка swapNode для разных значений
-    test_swapNode_equals(); //проверка swapNode для одинаковых значений
-    return 0;
 }
-//Unit tests
-
-int test_append_empty_list() {
-    Node* head1 = NULL;
-    append(&head1, "A");
-    assert((strcmp(head1->data, "A") == 0) && (head1->prev == NULL) && (head1->next == NULL));
+//Unit Tests
+int test_createLeaf() {
+    Tree* root = createLeaf("A");
+    assert(strcmp(root->data, "A")==0 && root->left == NULL && root->right == NULL);
 }
-int test_append_nonempty_list() {
-    Node* head2 = NULL;
-    append(&head2, "A");
-    append(&head2, "B");
-    assert((strcmp(head2->next->data, "B") == 0) && (head2->next->prev == head2) && (head2->next->next == NULL));
+int test_createTree_empty() {
+    Tree* root = NULL;
+    root=createTree(root, "A");
+    assert( strcmp(root->data, "A") == 0 && root->left == NULL && root->right == NULL );
 }
-int test_bubbleSort_empty_list() {
-    assert(!bubbleSort(NULL, false));
+int test_createTree_noempty_right() {
+    Tree* root = NULL;
+    root=createTree(root, "A");
+    root=createTree(root, "AA");
+    assert(strcmp(root->right->data, "AA") == 0 && root->right->left == NULL && root->right->right == NULL);
 }
-int test_bubbleSort() {
-    Node a = { "B", NULL,NULL };
-    Node b = { "A", &a,NULL };
-    Node c = { "C", &b,NULL };
-    a.next = &b;
-    b.next = &c;
-    Node* A = &a;
-    bubbleSort(&A, true);
-    assert((strcmp(A->data, "A") == 0) && (strcmp(A->next->data, "B") == 0) && (strcmp(A->next->next->data, "C") == 0));
+int test_createTree_noempty_left() {
+    Tree* root = NULL;
+    root = createTree(root, "AA");
+    root = createTree(root, "A");
+    assert(strcmp(root->left->data, "A") == 0 && root->left->left == NULL && root->left->right == NULL);
 }
-int test_bubbleSort_sorted_list() {
-    Node a = { "A", NULL,NULL };
-    Node b = { "B", &a,NULL };
-    a.next = &b;
-    Node* A = &a;
-    bubbleSort(&A, true);
-    assert((strcmp(A->data, "A") == 0) && (strcmp(A->next->data, "B") == 0));
+int test_Width_oneElement() {
+    Tree* root = NULL;
+    root = createTree(root, "A");
+    int w = Width(root);
+    assert(w==1);
 }
-int test_bubbleSort_cosorted_list() {
-    Node a = { "B", NULL,NULL };
-    Node b = { "A", &a,NULL };
-    a.next = &b;
-    Node* A = &a;
-    bubbleSort(&A, true);
-    assert((strcmp(A->data, "A") == 0) && (strcmp(A->next->data, "B") == 0));
+int test_Width_nononeElement() {
+    Tree* root = NULL;
+    root = createTree(root, "A");
+    root = createTree(root, "AA");
+    int w = Width(root);
+    assert(w == 3);
 }
-int test_bubbleSort_constant_list() {
-    Node a = { "A", NULL,NULL };
-    Node b = { "A", &a,NULL };
-    a.next = &b;
-    Node* A = &a;
-    bubbleSort(&A , true);
-    assert((strcmp(A->data, "A") == 0) && (strcmp(A->next->data, "A") == 0));
+int test_Width_nononeElement_leftList() {
+    Tree* root = NULL;
+    root = createTree(root, "AAA");
+    root = createTree(root, "AA");
+    root = createTree(root, "A");
+    int w = Width(root);
+    assert(w == 6);
 }
-int test_bubbleSort_one_element_list() {
-    Node a = { "A", NULL,NULL };
-    Node* A = &a;
-    bubbleSort(&A, true);
-    assert((strcmp(A->data, "A") == 0));
+int test_Width_nononeElement_rightList() {
+    Tree* root = NULL;
+    root = createTree(root, "A");
+    root = createTree(root, "AA");
+    root = createTree(root, "AAA");
+    int w = Width(root);
+    assert(w == 6);
 }
-int test_swapNode_nonequals() {
-    Node a = { "A", NULL,NULL };
-    Node b = { "B", &a,NULL };
-    a.next = &b;
-    swapNode(&a, a.next);
-    assert((strcmp(a.data, "B") == 0) && (strcmp(a.next->data, "A") == 0));
+int test_Width_nononeElement_FullTree() {
+    Tree* root = NULL;
+    root = createTree(root, "AA");
+    root = createTree(root, "AAA");
+    root = createTree(root, "A");
+    int w = Width(root);
+    assert(w == 6);
 }
-int test_swapNode_equals() {
-    Node a = { "A", NULL,NULL };
-    Node b = { "A", &a,NULL };
-    a.next = &b;
-    swapNode(&a, a.next);
-    assert((strcmp(a.data, "A") == 0) && (strcmp(a.next->data, "A") == 0));
+int test_Width_nononeElement_RandomTree() {
+    Tree* root = NULL;
+    root = createTree(root, "AA");
+    root = createTree(root, "AAA");
+    root = createTree(root, "A");
+    root = createTree(root, "AAAA");
+    int w = Width(root);
+    assert(w == 10);
 }
