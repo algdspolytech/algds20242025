7f7c58b389edabfa24b0c3493fa0d943add64ac9
Jarsky-s <daniilsanin2000@gmail.ru>
2025-01-07 01:35:06 +0300

    отредактированная версия maze_solver.c

--- lab3/maze_solver.c
+++ lab3/maze_solver.c
@@ -2,30 +2,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include "maze_solver.h"
 #define MAZE_X 4
 #define MAZE_Y 4
-enum{U = 1<<0, D = 1<<1, R = 1<<2, L = 1<<3, START = 1<<4, FINISH = 1<<5};
+enum {U = 1<<0, D = 1<<1, R = 1<<2, L = 1<<3, START = 1<<4, FINISH = 1<<5};
 
-typedef struct Node{
-    int x;
-    int y;
-}Node;
+typedef struct Node {
+    int x, y;
+} Node;
 
-typedef struct Path{
+typedef struct Path {
     int maxlen, head;
-    Node * path;
-}Path;
+    Node *path;
+} Path;
 
-// const int U = (1<<0);
-// const int D = (1<<1);
-// const int R = (1<<2);
-// const int L = (1<<3);
-
-// const int START = (1<<4);
-// const int FINISH = (1<<5);
-
-int maze[MAZE_X][MAZE_Y] = 
-{
+int maze[MAZE_X][MAZE_Y] = {
     {R|D|START, L, R|D, L},
     {U|R|D, L|R, U|D|L, D},
     {U|D, R|D, U|R|L, U|D|L},
@@ -35,113 +26,103 @@ int maze[MAZE_X][MAZE_Y] =
 Node start = {0, 0};
 Node finish = {3, 3};
 
-void free2Darray(int ** array){
-    for (int i = 0; i < MAZE_Y; i++){
-        free(array[i]);
-    }
-    free(array);
+int visited[MAZE_X][MAZE_Y] = {0};
+
+int nodeCompare(Node *n1, Node *n2) {
+    return n1->x == n2->x && n1->y == n2->y;
 }
 
-
-int nodeCompare(Node * n1, Node * n2){
-    if (n1->x == n2->x && n1->y == n2->y)
-        return true;
-    else
-        return false;
-}
-
-void pathSetup(Path * p, Node * n){
+void pathSetup(Path *p, Node *n) {
     p->maxlen = 10;
     p->head = 0;
     p->path = (Node*)malloc(p->maxlen * sizeof(Node));
-    p->path[p->head]= *n;
+    p->path[p->head] = *n;
 }
 
-void pathEmbiggen(Path * p){
+void pathEmbiggen(Path *p) {
     p->maxlen *= 2;
     p->path = (Node*)realloc(p->path, p->maxlen * sizeof(Node));
 }
 
-int pathTooSmall(Path * p){
+int pathTooSmall(Path *p) {
     return p->head >= p->maxlen - 1;
 }
 
-Node * pathHead(Path * p){
-    return &(p->path[p->head]);
-}
-
-void pathPush(Path * p, Node * n){
+void pathPush(Path *p, Node *n) {
     if (pathTooSmall(p))
         pathEmbiggen(p);
-    p->head += 1;
+    p->head++;
     p->path[p->head] = *n;
 }
 
-void pathPop(Path * p){
-    if (p->head > 0){
-        p->head -= 1;
-    }
+void pathPop(Path *p) {
+    if (p->head > 0)
+        p->head--;
 }
 
-int isValidMove(int x, int y){
+int isValidMove(int x, int y) {
     return x >= 0 && x < MAZE_X && y >= 0 && y < MAZE_Y;
 }
 
-int canMove(Node * current, int direction){
-    switch (direction)
-    {
-    case U: return isValidMove(current->x - 1, current->y) && (maze[current->x][current->y] & U);
-    case D: return isValidMove(current->x + 1, current->y) && (maze[current->x][current->y] & D);
-    case R: return isValidMove(current->x, current->y + 1) && (maze[current->x][current->y] & R);
-    case L: return isValidMove(current->x, current->y - 1) && (maze[current->x][current->y] & L);
-    default: return 0;
+int canMove(Node *current, int direction) {
+    switch (direction) {
+        case U: return isValidMove(current->x - 1, current->y) && (maze[current->x][current->y] & U);
+        case D: return isValidMove(current->x + 1, current->y) && (maze[current->x][current->y] & D);
+        case R: return isValidMove(current->x, current->y + 1) && (maze[current->x][current->y] & R);
+        case L: return isValidMove(current->x, current->y - 1) && (maze[current->x][current->y] & L);
+        default: return 0;
     }
 }
 
-int findPath(Path * path, Node * current){
-    if(nodeCompare(&finish, current)){
+int findPath(Path *path, Node *current) {
+    if (nodeCompare(&finish, current))
         return 1;
-    }
-    
-    for(int direction = U; direction <= L; direction <<= 1){
-        if(canMove(current, direction)){
-            Node next;
-            switch (direction)
-            {
-            case U: next.x = current->x - 1; next.y = current->y; break;
-            case D: next.x = current->x + 1; next.y = current->y; break;
-            case R: next.x = current->x; next.y = current->y + 1; break;
-            case L: next.x = current->x; next.y = current->y - 1; break;        
-            }
-        
-            pathPush(path, &next);
 
-            if(findPath(path, &next))
-                return 1;
-            
-            pathPop(path);
+    for (int direction = U; direction <= L; direction <<= 1) {
+        if (canMove(current, direction)) {
+            Node next = {current->x, current->y};
+            switch (direction) {
+                case U: next.x--; break;
+                case D: next.x++; break;
+                case R: next.y++; break;
+                case L: next.y--; break;
+            }
+
+            if (!visited[next.x][next.y]) {
+                visited[next.x][next.y] = 1;
+                pathPush(path, &next);
+
+                if (findPath(path, &next))
+                    return 1;
+
+                pathPop(path);
+                visited[next.x][next.y] = 0;
+            }
         }
     }
 
     return 0;
 }
 
-void printPath(Path * p){
-    for(int i = 0; i <= p->head; i++){
+void printPath(Path *p) {
+    for (int i = 0; i <= p->head; i++) {
         printf("(%d, %d) ", p->path[i].x, p->path[i].y);
     }
     printf("\n");
 }
 
-int main(){
+int main() {
     Path path;
     pathSetup(&path, &start);
-    if (findPath(&path, &start)){
+    visited[start.x][start.y] = 1;
+
+    if (findPath(&path, &start)) {
         printf("Path found\n");
         printPath(&path);
-    }
-    else
+    } else {
         printf("No Path found\n");
+    }
+
     free(path.path);
     return 0;
 }
