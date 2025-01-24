82edc8e1483f012cc78d6bad3b29f1bef6d536e0
Danila <gladiatorsho228@gmail.com>
2025-01-24 15:32:59 +0300

    Update test.c

--- LabC/test.c
+++ LabC/test.c
@@ -1,7 +1,7 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "gtest/gtest.h"
+#include <assert.h>
 #include "../LabC/Source.h"
 
 void TestwriteAdjacencyList_ValidGraph_no1() {
@@ -9,24 +9,22 @@ void TestwriteAdjacencyList_ValidGraph_no1() {
     graph[0][1] = 1;
     graph[1][2] = 1;
 
-
     writeAdjacencyList("test_list1.txt", graph, 3);
-
-    printf("TestwriteAdjacencyList_ValidGraph_no1 = 1\n"); 
+    FILE* file = fopen("test_list1.txt", "r");
+    assert(file != NULL);
+    fclose(file);
 }
-
 
 void TestwriteAdjacencyMatrix_ValidGraph_no2() {
     int graph[MAX_VERTICES][MAX_VERTICES] = { 0 };
     graph[0][1] = 1;
     graph[1][0] = 1;
 
-
     writeAdjacencyMatrix("test_matrix1.txt", graph, 2);
-
-    printf("TestwriteAdjacencyMatrix_ValidGraph_no1 = 1\n"); 
+    FILE* file = fopen("test_matrix1.txt", "r");
+    assert(file != NULL);
+    fclose(file);
 }
-
 
 void TestcompareGraphs_SameGraphs_no3() {
     int graph1[MAX_VERTICES][MAX_VERTICES] = { 0 };
@@ -38,9 +36,8 @@ void TestcompareGraphs_SameGraphs_no3() {
     graph2[0][1] = 1;
     graph2[1][0] = 1;
 
-
     int result = compareGraphs(graph1, graph2, 2);
-    printf("TestcompareGraphs_SameGraphs_no1 = %i\n", result == 1 ? 1 : 0);
+    assert(result == 1);
 }
 
 void TestcompareGraphs_DifferentGraphs_no4() {
@@ -52,18 +49,15 @@ void TestcompareGraphs_DifferentGraphs_no4() {
     graph2[1][2] = 1;
     graph2[2][1] = 1;
 
-
     int result = compareGraphs(graph1, graph2, 3);
-    printf("TestcompareGraphs_DifferentGraphs_no1 = %i\n", result == 0 ? 1 : 0);
+    assert(result == 0);
 }
 
 void TestgenerateRandomGraph_FullDensity_no5() {
     int graph[MAX_VERTICES][MAX_VERTICES] = { 0 };
-    float density = 1.0; 
-
+    float density = 1.0;
 
     generateRandomGraph(graph, 4, density);
-
 
     int connections = 0;
     for (int i = 0; i < 4; i++) {
@@ -72,13 +66,12 @@ void TestgenerateRandomGraph_FullDensity_no5() {
         }
     }
 
-    printf("TestgenerateRandomGraph_FullDensity_no5 = %i\n", connections == 12 ? 1 : 0);
+    assert(connections == 12); // Полная плотность (4 узла, 12 рёбер)
 }
-
 
 void TestgenerateRandomGraph_ZeroDensity_no6() {
     int graph[MAX_VERTICES][MAX_VERTICES] = { 0 };
-    float density = 0.0; 
+    float density = 0.0;
 
     generateRandomGraph(graph, 4, density);
 
@@ -89,12 +82,12 @@ void TestgenerateRandomGraph_ZeroDensity_no6() {
         }
     }
 
-    printf("TestgenerateRandomGraph_ZeroDensity_no6 = %i\n", connections == 0 ? 1 : 0);
+    assert(connections == 0); // Нулевая плотность
 }
 
 void TestgenerateRandomGraph_MidDensity_no7() {
     int graph[MAX_VERTICES][MAX_VERTICES] = { 0 };
-    float density = 0.5; 
+    float density = 0.5;
 
     generateRandomGraph(graph, 4, density);
 
@@ -105,26 +98,30 @@ void TestgenerateRandomGraph_MidDensity_no7() {
         }
     }
 
-    printf("TestgenerateRandomGraph_MidDensity_no7 = %i\n", connections > 0 ? 1 : 0);
+    assert(connections > 0 && connections <= 12); // Средняя плотность
 }
 
 void TestwriteAdjacencyList_EmptyGraph_no8() {
     int graph[MAX_VERTICES][MAX_VERTICES] = { 0 };
 
     writeAdjacencyList("test_list_empty.txt", graph, 0);
-    printf("TestwriteAdjacencyList_EmptyGraph_no1 = 1\n");
-
+    FILE* file = fopen("test_list_empty.txt", "r");
+    assert(file != NULL);
+    fclose(file);
 }
+
 void TestwriteAdjacencyMatrix_EmptyGraph_no9() {
     int graph[MAX_VERTICES][MAX_VERTICES] = { 0 };
 
     writeAdjacencyMatrix("test_matrix_empty.txt", graph, 0);
-    printf("TestwriteAdjacencyMatrix_EmptyGraph_no1 = 1\n"); 
+    FILE* file = fopen("test_matrix_empty.txt", "r");
+    assert(file != NULL);
+    fclose(file);
 }
 
 void TestgenerateRandomGraph_DensityCheck_no10() {
     int graph[MAX_VERTICES][MAX_VERTICES] = { 0 };
-    float density = 0.8; 
+    float density = 0.8;
 
     generateRandomGraph(graph, 5, density);
 
@@ -136,14 +133,11 @@ void TestgenerateRandomGraph_DensityCheck_no10() {
         }
     }
 
-    int max_edges = 5 * (5 - 1) / 2; 
-    int expected_min_edges = max_edges * density; 
+    int max_edges = 5 * (5 - 1) / 2;
+    int expected_min_edges = max_edges * density;
 
-    printf("TestgenerateRandomGraph_DensityCheck_no10 = %i\n", connections >= expected_min_edges ? 1 : 0);
+    assert(connections >= expected_min_edges);
 }
-
-
-
 
 void RunAllTests() {
     TestwriteAdjacencyList_ValidGraph_no1();
@@ -156,6 +150,7 @@ void RunAllTests() {
     TestwriteAdjacencyList_EmptyGraph_no8();
     TestwriteAdjacencyMatrix_EmptyGraph_no9();
     TestgenerateRandomGraph_DensityCheck_no10();
+    printf("Все тесты успешно пройдены!\n");
 }
 
 int main() {
