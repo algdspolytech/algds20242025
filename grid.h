ed5c215af8f4656829791c9758546f2269b4d4c4
Egor <lesin.scl@gmail.com>
2025-02-23 23:27:07 +0300

    Выполненная LabC

---
+++ grid.h
@@ -0,0 +1,15 @@
+﻿#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+	int isAvailable(int, int, int, int, int**, int**);
+	void DFS(int, int, int, int, int**, int**);
+	int countDisconnectedAreas(int, int, int**, int**);
+	void printGrid(int, int, int, int**);
+#ifdef __cplusplus
+}
+#endif
