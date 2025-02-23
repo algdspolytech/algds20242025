3ee103ab0c3ad8f7da39e393d42c1f33a58c2757
Egor <lesin.scl@gmail.com>
2025-02-23 10:37:59 +0300

    Выполненная LabA

---
+++ FunctList.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	int day;
+	int month;
+	int year;
+} Date;
+
+typedef struct Node {
+	int hours;
+	Date date;
+	char name[20];
+	char surname[20];
+	struct Node* next;
+} Node;
+
+typedef struct HourCounter {
+	Date date;
+	char* name;
+	char* surname;
+	int total;
+} HourCounter;
+
+#ifdef __cplusplus
+
+extern "C" {
+#endif
+
+	char* fullname(char* surname, char* name);
+	Node* add_sort_list(Node* head, Node* New);
+	Node* parse_line(char* line);
+	Node* read_file(char* filename);
+	void print_filtered_list(Node* head, int N);
+	HourCounter* count_hours(Node* head, int* count);
+	void free_all(Node** head);
+
+#ifdef __cplusplus
+}
+#endif
