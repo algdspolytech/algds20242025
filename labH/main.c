405de8642f8696709e96c2122d246a860f0e7969
YourHumbleServant <andrewivanovviktorovich@gmail.com>
2025-03-21 18:51:39 +0300

    labH is done

---
+++ labH/main.c
@@ -0,0 +1,18 @@
+#include "functions.h"
+#include "tests.h"
+
+
+int main() {
+    test_avl_insert_ascending();
+    test_avl_insert_random();
+    test_avl_delete_leaf();
+    test_avl_delete_node_with_two_children();
+    test_avl_search_existing();
+    test_avl_search_non_existing();
+    test_avl_balance_after_insertions();
+    test_avl_duplicate_insert();
+    test_avl_complex_operations();
+    
+    printf("All tests passed!\n");
+    return 0;
+}
