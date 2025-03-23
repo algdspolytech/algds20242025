60e3700de05b24ab35a290b1795879384cb426c4
Shekhurin KA <k.shekhurin@gmail.com>
2025-03-23 19:19:02 +0300

    finished lab H

--- libs/tree/include/tree.h
+++ libs/tree/include/tree.h
@@ -2,18 +2,43 @@
 #define TREE_H
 #include<stdio.h>
 
+typedef enum color {
+    BLACK = 0,
+    RED = 1
+} color_t;
+
+typedef enum  direction {
+    LEFT = 0,
+    RIGHT = 1
+} direction_t;
+
 typedef struct Node {
-    int id;
+    struct Node* parent;
+
+    color_t color;
+    struct Node* left;
+    struct Node* right;
+
     int data;
-    int reqCnt;
+} node_t;
 
-    struct Node *parent;
-    struct Node *left;
-    struct Node *right;
-} TREE_Node_t;
+typedef struct Tree {
+    struct Node* root;
+    struct Node* nil;
+} tree_t;
 
+tree_t* tree_create();
+node_t* create_node(int data, color_t color, node_t *nil);
 
-TREE_Node_t* TREE_CreateTreeFromFile(FILE* file);
-void TREE_DeleteTree(TREE_Node_t* root);
+node_t* findLUB(int upper_bound, tree_t* tree);
+
+direction_t get_direction(node_t* node);
+
+void dispose_tree(tree_t* tree);
+
+void rotate(tree_t* tree, node_t* node);
+void insert(tree_t* tree, node_t* node);
+void fix_violations(tree_t *tree, node_t *new_node);
+void rb_remove(tree_t* tree, node_t* node);
 
 #endif //TREE_H
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,309 +1,274 @@
-#include <stdio.h>
 #include <gtest/gtest.h>
 
 extern "C" {
-#include <tree.h>
+#include "tree.h"
 }
 
-FILE* tmp(const char* name, const char* data) {
-    FILE* f = fopen(name, "w");
-    fprintf(f, "%s", data);
-    fclose(f);
+bool is_valid_rb_tree(tree_t *tree, node_t *node, int black_count, int current_count) {
+    if (node == tree->nil) return black_count == current_count;
 
-    f = fopen(name, "r");
-    return f;
+    if (node->color == BLACK) current_count++;
+    if (node->parent && node->color == RED && node->parent->color == RED) return false;
+
+    return is_valid_rb_tree(tree, node->left, black_count, current_count) &&
+           is_valid_rb_tree(tree, node->right, black_count, current_count);
 }
 
-TEST(CreateTreeFromFile, TreeNo1) {
-    FILE* f1 = tmp("tree1.txt",
-    "3 \
-         10 20 30 \
-         0 1 1 \
-         1 0 2 \
-         2 0 -1");
+bool validate_rb_tree(tree_t* tree) {
+    if (!tree->root) return true;
+    int cnt = 0;
+    node_t *cur = tree->root;
 
-    /*
-    #       30(6)
-    #      /
-    #    20(4)
-    #   /
-    # 10(2)
-    */
-
-    std::vector<int> expectedReq = {2, 4, 6};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    std::vector<TREE_Node_t*> stack;
-    stack.push_back(root);
-
-    while(!stack.empty()) {
-        TREE_Node_t* node = stack.back();
-        stack.pop_back();
-
-        assert(node->reqCnt == expectedReq[node->id]);
-
-        if(node->left != NULL)
-            stack.push_back(node->left);
-        if(node->right != NULL)
-            stack.push_back(node->right);
+    while(cur != tree->nil) {
+        if (cur->color == BLACK)
+            cnt++;
+        cur = cur->left;
     }
+
+    return is_valid_rb_tree(tree, tree->root, cnt, 0);
 }
 
-TEST(CreateTreeFromFile, TreeNo2) {
-    FILE* f1 = tmp("tree1.txt",
-    "5\
-    1 2 3 4 5\
-    0 1 2\
-    1 0 2\
-    2 1 4\
-    3 0 4\
-    4 0 -1");
+TEST(RBTreeTests, Insert_RootShouldBeBlack_No1) {
+    tree_t *tree = tree_create();
+    node_t *node = create_node(10, RED, tree->nil);
+    insert(tree, node);
 
-    /*
-    #         5(5)
-    #       /   \
-    #     3(3)    4(1)
-    #    /    \
-    #   1(1)   2(1)
-    */
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(tree->root->color, BLACK);
+    EXPECT_EQ(tree->root->parent, tree->nil);
 
-    std::vector<int> expectedReq = {1, 1, 3, 1, 5};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
+    
+}
 
-    std::vector<TREE_Node_t*> stack;
-    stack.push_back(root);
+TEST(RBTreeTests, Fix_violations_ParentIsBlackNoFixNeeded_No2) {
+    tree_t *tree = tree_create();
+    node_t *root = create_node(20, BLACK, tree->nil);
+    node_t *child = create_node(10, RED, tree->nil);
 
-    while(!stack.empty()) {
-        TREE_Node_t* node = stack.back();
-        stack.pop_back();
+    tree->root = root;
+    root->left = child;
+    child->parent = root;
 
-        assert(node->reqCnt == expectedReq[node->id]);
+    fix_violations(tree, child);
 
-        if(node->left != NULL)
-            stack.push_back(node->left);
-        if(node->right != NULL)
-            stack.push_back(node->right);
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(child->color, RED);
+    EXPECT_EQ(child->parent, root);
+
+    dispose_tree(tree);
+}
+
+TEST(RBTreeTests, Fix_violations_ParentAndUncleAreRed_No3) {
+    tree_t *tree = tree_create();
+    node_t *root = create_node(30, BLACK, tree->nil);
+    node_t *left_child = create_node(20, RED, tree->nil);
+    node_t *right_child = create_node(40, RED, tree->nil);
+    node_t *grandchild = create_node(10, RED, tree->nil);
+
+    tree->root = root;
+    root->left = left_child;
+    left_child->parent = root;
+    root->right = right_child;
+    right_child->parent = root;
+    left_child->left = grandchild;
+    grandchild->parent = left_child;
+
+    fix_violations(tree, grandchild);
+
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(root->color, BLACK);
+    EXPECT_EQ(left_child->color, BLACK);
+    EXPECT_EQ(right_child->color, BLACK);
+    EXPECT_EQ(grandchild->parent, left_child);
+    dispose_tree(tree);
+}
+
+TEST(RBTreeTests, fix_violations_RightChildOfLeftParentRotatesLeft_No4) {
+    tree_t *tree = tree_create();
+    node_t *root = create_node(30, BLACK, tree->nil);
+    node_t *left_child = create_node(20, RED, tree->nil);
+    node_t *right_child_of_left = create_node(25, RED, tree->nil);
+
+    tree->root = root;
+    root->left = left_child;
+    left_child->parent = root;
+    left_child->right = right_child_of_left;
+    right_child_of_left->parent = left_child;
+
+    fix_violations(tree, right_child_of_left);
+
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(tree->root, right_child_of_left);
+    EXPECT_EQ(tree->root->color, BLACK);
+    EXPECT_EQ(tree->root->left, left_child);
+    EXPECT_EQ(tree->root->right, root);
+    dispose_tree(tree);
+}
+
+TEST(RBTreeTests, fix_violations_LeftChildOfLeftParentRotatesRight_No5) {
+    tree_t *tree = tree_create();
+    node_t *root = create_node(30, BLACK, tree->nil);
+    node_t *left_child = create_node(20, RED, tree->nil);
+    node_t *left_left_child = create_node(10, RED, tree->nil);
+
+    tree->root = root;
+    root->left = left_child;
+    left_child->parent = root;
+    left_child->left = left_left_child;
+    left_left_child->parent = left_child;
+
+    fix_violations(tree, left_left_child);
+
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(tree->root->left, left_left_child);
+    EXPECT_EQ(tree->root->color, BLACK);
+    EXPECT_EQ(tree->root->right, root);
+    dispose_tree(tree);
+}
+
+TEST(RBTreeTests, Insert_MultipleInsertsShouldMaintainRedBlackProperties_No6) {
+    tree_t *tree = tree_create();
+    int values[] = {50, 30, 70, 20, 40, 60, 80, 10, 25, 35, 45, 33, 24, 66, 78};
+
+    for (int v : values) {
+        node_t* node = create_node(v, RED, tree->nil);
+        insert(tree, node);
     }
+
+    EXPECT_TRUE(validate_rb_tree(tree));
+    dispose_tree(tree);
 }
 
-TEST(CreateTreeFromFile, TreeNo3) {
-    FILE* f1 = tmp("tree1.txt",
-    "1\
-    100\
-    0 0 -1");
+TEST(RBTreeTests, rb_remove_DeleteLeafNode_No7) {
+    tree_t *tree = tree_create();
+    tree->root = create_node(10, BLACK, tree->nil);
+    tree->root->left = create_node(5, RED, tree->nil);
+    tree->root->right = create_node(15, RED, tree->nil);
+    tree->root->left->parent = tree->root;
+    tree->root->right->parent = tree->root;
 
-    /*
-    # 100(3)
-    */
+    Node *nodeToDelete = findLUB(5, tree);
+    rb_remove(tree, nodeToDelete);
 
-    std::vector<int> expectedReq = {3};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    std::vector<TREE_Node_t*> stack;
-    stack.push_back(root);
-
-    while(!stack.empty()) {
-        TREE_Node_t* node = stack.back();
-        stack.pop_back();
-
-        assert(node->reqCnt == expectedReq[node->id]);
-
-        if(node->left != NULL)
-            stack.push_back(node->left);
-        if(node->right != NULL)
-            stack.push_back(node->right);
-    }
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(tree->root->left, tree->nil);
+    EXPECT_EQ(tree->root->data, 10);
+    EXPECT_EQ(tree->root->color, BLACK);
+    dispose_tree(tree);
 }
 
-TEST(CreateTreeFromFile, TreeNo4) {
-    FILE* f1 = tmp("tree1.txt",
-    "7\
-    5 10 15 20 25 30 35\
-    0 1 2\
-    1 0 2\
-    2 1 6\
-    3 1 4\
-    5 0 4\
-    4 0 6\
-    6 0 -1");
+TEST(RBTreeTests, rb_remove_DeleteNodeWithOneChild_No8) {
+    tree_t *tree = tree_create();
+    tree->root = create_node(10, BLACK, tree->nil);
+    tree->root->left = create_node(5, BLACK, tree->nil);
+    tree->root->right = create_node(15, BLACK, tree->nil);
+    tree->root->left->right = create_node(7, RED, tree->nil);
+    tree->root->left->parent = tree->root;
+    tree->root->right->parent = tree->root;
+    tree->root->left->right->parent = tree->root->left;
 
-    /*
-    #          35(13)
-    #       /         \
-    #     15(5)        25(6)
-    #    /    \        /    \
-    #   5(1)   10(2)  20(2) 30(2)
-    #
-    */
+    node_t *nodeToDelete = findLUB(5, tree);
+    rb_remove(tree, nodeToDelete);
 
-    std::vector<int> expectedReq = {1, 2, 5, 2, 6, 2, 13};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    std::vector<TREE_Node_t*> stack;
-    stack.push_back(root);
-
-    while(!stack.empty()) {
-        TREE_Node_t* node = stack.back();
-        stack.pop_back();
-
-        assert(node->reqCnt == expectedReq[node->id]);
-
-        if(node->left != NULL)
-            stack.push_back(node->left);
-        if(node->right != NULL)
-            stack.push_back(node->right);
-    }
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(tree->root->left->data, 7);
+    EXPECT_EQ(tree->root->left->color, BLACK);
+    dispose_tree(tree);
 }
 
-TEST(CreateTreeFromFile, TreeNo5) {
-    FILE* f1 = tmp("tree1.txt",
-    "0");
+TEST(RBTreeTests, rb_remove_DeleteNodeWithTwoChildren_No9) {
+    tree_t *tree = tree_create();
+    tree->root = create_node(10, BLACK, tree->nil);
+    tree->root->left = create_node(5, BLACK, tree->nil);
+    tree->root->right = create_node(15, BLACK, tree->nil);
+    tree->root->left->left = create_node(3, RED, tree->nil);
+    tree->root->left->right = create_node(7, RED, tree->nil);
+    tree->root->left->parent = tree->root;
+    tree->root->right->parent = tree->root;
+    tree->root->left->left->parent = tree->root->left;
+    tree->root->left->right->parent = tree->root->left;
 
-    /*
-    # Empty
-    */
+    Node *nodeToDelete = findLUB(5, tree);
+    rb_remove(tree, nodeToDelete);
 
-    std::vector<int> expectedReq = {0};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    assert(root == NULL);
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(tree->root->left->data, 7);
+    EXPECT_EQ(tree->root->left->color, BLACK);
+    dispose_tree(tree);
 }
 
-TEST(CreateTreeFromFile, TreeNo6) {
-    FILE* f1 = tmp("tree1.txt",
-    "4\
-    10 20 30 40\
-    0 0 1\
-    1 0 2\
-    2 0 3\
-    3 0 -1");
+TEST(RBTreeTests, rb_remove_DeleteRootNode_No10) {
+    tree_t *tree = tree_create();
+    tree->root = create_node(10, BLACK, tree->nil);
+    tree->root->left = create_node(5, RED, tree->nil);
+    tree->root->right = create_node(15, RED, tree->nil);
+    tree->root->left->parent = tree->root;
+    tree->root->right->parent = tree->root;
 
-    /*
-    # 40(8)
-    #  \
-    #   30(6)
-    #    \
-    #     20(4)
-    #      \
-    #       10(2)
-    */
+    Node *nodeToDelete = findLUB(10, tree);
+    rb_remove(tree, nodeToDelete);
 
-    std::vector<int> expectedReq = {2, 4, 6, 8};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    std::vector<TREE_Node_t*> stack;
-    stack.push_back(root);
-
-    while(!stack.empty()) {
-        TREE_Node_t* node = stack.back();
-        stack.pop_back();
-
-        assert(node->reqCnt == expectedReq[node->id]);
-
-        if(node->left != NULL)
-            stack.push_back(node->left);
-        if(node->right != NULL)
-            stack.push_back(node->right);
-    }
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_NE(tree->root, tree->nil);
+    EXPECT_TRUE(tree->root->data == 15 || tree->root->data == 5);
+    EXPECT_EQ(tree->root->color, BLACK);
+    dispose_tree(tree);
 }
 
-TEST(CreateTreeFromFile, TreeNo7) {
-    FILE* f1 = tmp("tree1.txt",
-    "6\
-    3 -6 9 12 15 18\
-    0 1 2\
-    1 0 2\
-    2 1 4\
-    3 0 4\
-    4 1 5\
-    5 0 -1");
+TEST(RBTreeTests, rb_remove_DeleteNodeCausingFixup_No11) {
+    tree_t *tree = tree_create();
+    tree->root = create_node(10, BLACK, tree->nil);
+    tree->root->left = create_node(5, BLACK, tree->nil);
+    tree->root->right = create_node(20, BLACK, tree->nil);
+    tree->root->right->left = create_node(15, RED, tree->nil);
+    tree->root->right->right = create_node(25, RED, tree->nil);
+    tree->root->left->parent = tree->root;
+    tree->root->right->parent = tree->root;
+    tree->root->right->left->parent = tree->root->right;
+    tree->root->right->right->parent = tree->root->right;
 
-    /*
-    #         18(10)
-    #       /
-    #     15(8)
-    #     /   \
-    #   9(4)   12(2)
-    #  /   \
-    # 3(1)  -6(2)
-    */
+    node_t *nodeToDelete = findLUB(25, tree);
+    rb_remove(tree, nodeToDelete);
 
-    std::vector<int> expectedReq = {1, 2, 4, 2, 8, 10};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    std::vector<TREE_Node_t*> stack;
-    stack.push_back(root);
-
-    while(!stack.empty()) {
-        TREE_Node_t* node = stack.back();
-        stack.pop_back();
-
-        assert(node->reqCnt == expectedReq[node->id]);
-
-        if(node->left != NULL)
-            stack.push_back(node->left);
-        if(node->right != NULL)
-            stack.push_back(node->right);
-    }
+    EXPECT_TRUE(validate_rb_tree(tree));
+    EXPECT_EQ(findLUB(25, tree), tree->nil);
+    dispose_tree(tree);
 }
 
-TEST(CreateTreeFromFile, InvalidBecauseOfCycleNo8) {
-    FILE* f1 = tmp("tree1.txt",
-    "3\
-    10 20 30\
-    0 1 1\
-    1 0 2\
-    2 1 0");
+TEST(RBTreeTests, findLUB_FindLUB_No12) {
+    tree_t *tree = tree_create();
+    tree->root = create_node(10, BLACK, tree->nil);
+    tree->root->left = create_node(5, BLACK, tree->nil);
+    tree->root->right = create_node(15, BLACK, tree->nil);
+    tree->root->left->left = create_node(3, RED, tree->nil);
+    tree->root->left->right = create_node(7, RED, tree->nil);
+    tree->root->right->left = create_node(12, RED, tree->nil);
+    tree->root->right->right = create_node(20, RED, tree->nil);
 
-    /*
-    # Empty
-    */
-
-    std::vector<int> expectedReq = {};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    assert(root == NULL);
+    EXPECT_EQ(findLUB(6, tree)->data, 7);
+    EXPECT_EQ(findLUB(10, tree)->data, 10);
+    EXPECT_EQ(findLUB(14, tree)->data, 15);
+    EXPECT_EQ(findLUB(2, tree)->data, 3);
+    EXPECT_EQ(findLUB(20, tree)->data, 20);
+    dispose_tree(tree);
 }
 
-TEST(CreateTreeFromFile, InvalidBecauseOfDoubleAssignNo9) {
-    FILE* f1 = tmp("tree1.txt",
-    "4\
-    10 20 30 40\
-    0 1 1\
-    1 0 2\
-    1 1 3\
-    3 0 -1");
+TEST(RBTreeTests, findLUB_FindLUBReturnsNil_No13) {
+    tree_t *tree = tree_create();
+    tree->root = create_node(10, BLACK, tree->nil);
+    tree->root->left = create_node(5, BLACK, tree->nil);
+    tree->root->right = create_node(15, BLACK, tree->nil);
+    tree->root->left->left = create_node(3, RED, tree->nil);
+    tree->root->left->right = create_node(7, RED, tree->nil);
+    tree->root->right->left = create_node(12, RED, tree->nil);
+    tree->root->right->right = create_node(20, RED, tree->nil);
 
-    /*
-    # Empty
-    */
-
-    std::vector<int> expectedReq = {};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    assert(root == NULL);
-}
-
-TEST(CreateTreeFromFile, InvalidBecauseOfAssignToTheSameEdgeNo9) {
-    FILE* f1 = tmp("tree1.txt",
-    "4\
-    10 20 30 40\
-    0 1 1\
-    1 1 2\
-    2 1 1\
-    3 0 -1");
-
-    /*
-    # Empty
-    */
-
-    std::vector<int> expectedReq = {};
-    TREE_Node_t* root = TREE_CreateTreeFromFile(f1);
-
-    assert(root == NULL);
+    EXPECT_EQ(findLUB(25, tree), tree->nil);
+    EXPECT_EQ(findLUB(21, tree), tree->nil);
+    dispose_tree(tree);
 }
 
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
