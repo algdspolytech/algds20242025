e95173b0f884e7eca52c2cf0ebd8b0b95e87fdfb
MikailAbyshov <abyshovmikail@mail.ru>
2025-05-03 17:40:55 +0300

    LabJ completed

--- lab1/main.c
+++ lab1/main.c
@@ -1,156 +1,141 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "labH.h"
+#include <string.h>
 
-// Функция для создания нового узла
-SplayNode *newNode(int key) {
-  SplayNode *node = (SplayNode *)malloc(sizeof(SplayNode));
-  node->key = key;
-  node->left = node->right = NULL;
-  return node;
+#define TABLE_SIZE 1000
+
+typedef struct Node {
+    char *key;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    Node **buckets;
+    size_t size;
+} HashTable;
+
+unsigned int hash(const char *key) {
+    unsigned int hash_value = 0;
+    while (*key) {
+        hash_value = (hash_value * 31) + *key;
+        key++;
+    }
+    return hash_value % TABLE_SIZE;
 }
 
-// Функция для правого вращения
-SplayNode *rightRotate(SplayNode *y) {
-  SplayNode *x = y->left;
-  SplayNode *T2 = x->right;
-
-  // Выполнение вращения
-  x->right = y;
-  y->left = T2;
-
-  return x;
+void initTable(HashTable *table) {
+    table->buckets = (Node **)calloc(TABLE_SIZE, sizeof(Node *));
+    table->size = TABLE_SIZE;
 }
 
-// Функция для левого вращения
-SplayNode *leftRotate(SplayNode *x) {
-  SplayNode *y = x->right;
-  SplayNode *T2 = y->left;
-
-  // Выполнение вращения
-  y->left = x;
-  x->right = T2;
-
-  return y;
+Node *createNode(const char *key) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode) {
+        newNode->key = strdup(key);
+        newNode->next = NULL;
+    }
+    return newNode;
 }
 
-// Функция Splay: Перемещает узел с ключом 'key' в корень дерева.
-SplayNode *splay(SplayNode *root, int key) {
-  if (root == NULL || root->key == key)
-    return root;
+void addNode(HashTable *table, const char *key) {
+    unsigned int index = hash(key);
+    Node *newNode = createNode(key);
+    if (!newNode) return;
 
-  // Ключ находится в левом поддереве
-  if (key < root->key) {
-    // Ключ не найден в левом поддереве
-    if (root->left == NULL)
-      return root;
-
-    // Zig-Zig (Left Left)
-    if (key < root->left->key) {
-      root->left->left = splay(root->left->left, key);
-      root = rightRotate(root);
-    } else if (key > root->left->key) { // Zig-Zag (Left Right)
-      root->left->right = splay(root->left->right, key);
-      if (root->left->right != NULL)
-        root->left = leftRotate(root->left);
+    if (!table->buckets[index]) {
+        table->buckets[index] = newNode;
+        return;
     }
 
-    return (root->left == NULL) ? root : rightRotate(root);
-  } else { // Ключ находится в правом поддереве
-    // Ключ не найден в правом поддереве
-    if (root->right == NULL)
-      return root;
-
-    // Zag-Zig (Right Left)
-    if (key < root->right->key) {
-      root->right->left = splay(root->right->left, key);
-      if (root->right->left != NULL)
-        root->right = rightRotate(root->right);
-    } else if (key > root->right->key) { // Zag-Zag (Right Right)
-      root->right->right = splay(root->right->right, key);
-      root = leftRotate(root);
+    Node *current = table->buckets[index];
+    Node *prev = NULL;
+    while (current) {
+        if (strcmp(current->key, key) == 0) {
+            free(newNode->key);
+            free(newNode);
+            return;
+        }
+        prev = current;
+        current = current->next;
     }
 
-    return (root->right == NULL) ? root : leftRotate(root);
-  }
+    prev->next = newNode;
 }
 
-// Функция поиска:  Перемещает узел с ключом 'key' в корень дерева и возвращает его.
-SplayNode *search(SplayNode *root, int key) {
-  return splay(root, key);
+int findNode(HashTable *table, const char *key) {
+    unsigned int index = hash(key);
+    Node *current = table->buckets[index];
+
+    while (current) {
+        if (strcmp(current->key, key) == 0) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
 }
 
-// Функция вставки узла с ключом 'key'
-SplayNode *insert(SplayNode *root, int key) {
-  SplayNode *node = newNode(key);
+void deleteNode(HashTable *table, const char *key) {
+    unsigned int index = hash(key);
+    Node *current = table->buckets[index];
+    Node *prev = NULL;
 
-  if (root == NULL) {
-    return node;
-  }
-
-  // Splay для ключа, чтобы корень был ближайшим к ключу вставки.
-  root = splay(root, key);
-
-  // Если ключ уже существует, ничего не вставляем. Можно изменить логику.
-  if (root->key == key) {
-    free(node);
-    return root; // Или  return root; // Можно изменить логику.
-  }
-
-  if (key < root->key) {
-    node->right = root;
-    node->left = root->left;
-    root->left = NULL;
-  } else {
-    node->left = root;
-    node->right = root->right;
-    root->right = NULL;
-  }
-  return node;
+    while (current) {
+        if (strcmp(current->key, key) == 0) {
+            if (prev) {
+                prev->next = current->next;
+            } else {
+                table->buckets[index] = current->next;
+            }
+            free(current->key);
+            free(current);
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
 }
 
-// Функция удаления узла с ключом 'key'
-SplayNode *deleteNode(SplayNode *root, int key) {
-  SplayNode *temp;
-
-  if (root == NULL)
-    return NULL;
-
-  // Splay для ключа
-  root = splay(root, key);
-
-  // Если ключ не найден
-  if (root->key != key)
-    return root;
-
-  // Если ключ найден
-  if (root->left == NULL) {
-    temp = root;
-    root = root->right;
-  } else {
-    temp = root;
-    root = splay(root->left, key); // Splay максимальный элемент в левом поддереве
-    root->right = temp->right;
-  }
-
-  free(temp);
-  return root;
+void freeList(Node *head) {
+    Node *tmp;
+    while (head) {
+        tmp = head;
+        head = head->next;
+        free(tmp->key);
+        free(tmp);
+    }
 }
 
-// Функция для inorder обхода дерева (для отладки)
-void inorder(SplayNode *root) {
-  if (root != NULL) {
-    inorder(root->left);
-    printf("%d ", root->key);
-    inorder(root->right);
-  }
+void freeHashTable(HashTable *table) {
+    for (size_t i = 0; i < table->size; i++) {
+        if (table->buckets[i]) {
+            freeList(table->buckets[i]);
+        }
+    }
+    free(table->buckets);
 }
 
-// Функция для освобождения памяти, занимаемой деревом
-void freeTree(SplayNode *root) {
-  if (root != NULL) {
-    freeTree(root->left);
-    freeTree(root->right);
-    free(root);
-  }
+int main(void) {
+    HashTable table;
+    initTable(&table);
+
+    char ch[2];
+    char key[133];
+    while (scanf("%1s %132s", ch, key) == 2) {
+        switch (ch[0]) {
+        case 'a':
+            addNode(&table, key);
+            break;
+        case 'r':
+            deleteNode(&table, key);
+            break;
+        case 'f':
+            printf(findNode(&table, key) ? "yes\n" : "no\n");
+            break;
+        default:
+            break;
+        }
+    }
+    freeHashTable(&table);
+    return 0;
 }
