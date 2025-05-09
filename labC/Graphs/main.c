c09fbb0eb6fc51647ffb7e33315856d59cff24b9
”Tigran12321” <bagradilarionov@gmail.com>
2025-05-09 20:15:58 +0300

    мяу

--- labC/Graphs/main.c
+++
@@ -1,9 +0,0 @@
-#include "code.h"
-#include "test.h"
-
-
-int main()
-{
-    run_all_tests();
-    return 0;
-}
--- labF/Tree/test.h
+++
@@ -1,8 +0,0 @@
-#ifndef test_h
-#define test_h
-
-#include <assert.h>
-#include "code.h"
-
-
-#endif
